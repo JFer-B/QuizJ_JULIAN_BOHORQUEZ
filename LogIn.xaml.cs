4fa6fe2b61c70d725678304838e2b167649c26c6
JFer-B <bohorquez.julianf@gmail.com>
2020-08-30 19:45:17 -0500

    Agregar archivos de proyecto.

---
+++ LogIn.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventParadigmExample
+{
+    /// <summary>
+    /// Lógica de interacción para LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Page
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLogIn_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(txtUsername.Text=="JFerk" && txtPassword.Password=="1234")
+            {
+                MainWindow w = (MainWindow) Window.GetWindow(this);
+                w.frameMain.NavigationService.Navigate(new Home());
+            }
+            else
+            {
+                MessageBox.Show("Error: Inavlid crediantials.");
+                txtUsername.Clear();
+                txtPassword.Clear();
+            }
+        }
+    }
+}
